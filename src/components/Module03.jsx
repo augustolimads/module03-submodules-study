fe095ff20f70be2ad90304df57d87244873dc72d
Augusto Lima <augustolimads@gmail.com>
2021-10-20 23:39:31 -0300

    add styling changes

--- src/components/Module03.jsx
+++ src/components/Module03.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import style from "./style.module.css";
 
-export const Module03 = () => {
+export const Module03 = ({ counter }) => {
   return (
     <div className={style.card}>
       <h1>MODULE 03</h1>
+      <p className={style.display}>{counter}</p>
     </div>
   );
 };
